86bf527cf309025c81cc276d84b2f3050d09d36a
PaoloFabris98 <f98paolo@gmail.com>
2024-11-19 22:20:00 +0100

    feat:string format

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,7 +1,9 @@
 package org.lessons.java.shop;
 
+import java.text.Format;
 import java.util.ArrayList;
 import org.lessons.java.shop.Prodotto;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +12,7 @@ public class Main {
         String[] description = { "Pulizia Casa", "Frutta", "Prodotti per la Colazione" };
         Double[] prezzo = { 3.4, 5.2, 8.9 };
         Double[] iva = { 22.0, 4.0, 18.0 };
+        DecimalFormat df = new DecimalFormat("#.00");
 
         for (int i = 0; i < items.length; i++) {
             shopProduct.add(new Prodotto(items[i], description[i], prezzo[i], iva[i]));
@@ -17,12 +20,11 @@ public class Main {
             System.out.println(currentItem.codice);
             System.out.println(currentItem.nome);
             System.out.println("Descrizione prodotto: " + currentItem.descrizione);
-            System.out.println("Prezzo: " + currentItem.showPryce() + " euro");
-            System.out.println("Iva: " + currentItem.iva + "%");
-            System.out.println("Prezzo Ivato: " + currentItem.showPryceTaxed() + " euro");
+            System.out.println("Prezzo: " + df.format(currentItem.showPryce()) + " euro");
+            System.out.println("Iva: " + df.format(currentItem.iva) + "%");
+            System.out.println("Prezzo Ivato: " + df.format(currentItem.showPryceTaxed()) + " euro");
             System.out.println(currentItem.codeName());
             System.out.println("\n");
         }
-
     }
 }
